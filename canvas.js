2bcbbd1ad790c7b4db73c19d5f2276ef28b0ae1d
Jbausc <jake.bausch2002@gmail.com>
2021-09-14 18:14:03 +1000

    asfg

--- canvas.js
+++ canvas.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d")
 
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0,0,150,75)
 
     let painting = false;
 
