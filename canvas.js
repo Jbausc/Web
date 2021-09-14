6eafc7a781a70c60e15b4b398180dde7521de4b1
Jbausc <jake.bausch2002@gmail.com>
2021-09-14 16:20:19 +1000

    t

--- canvas.js
+++ canvas.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext('2d');
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d')
 
-    canvas.height=window.innerHeight;
-    canvas.width=window.innerWidth;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10,20,150,50);
 
-    ctx.fillRect(100,100,200,500)
+
 })
 
