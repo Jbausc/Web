150e39c3483b72886b3391e3f90955eabd98cc95
Jbausc <jake.bausch2002@gmail.com>
2021-09-14 20:43:43 +1000

    iwahfi

--- canvas.js
+++ canvas.js
@@ -31,5 +31,23 @@ window.addEventListener("load", () => {
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
     canvas.addEventListener("mousemove", draw)
+
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      
+
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
 })
 
