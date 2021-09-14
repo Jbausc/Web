19ad1165f20bb49500e6b568ca1b7d05ce730861
Jbausc <jake.bausch2002@gmail.com>
2021-09-14 16:25:49 +1000

    d

--- canvas.js
+++ canvas.js
@@ -1,10 +1,31 @@
 window.addEventListener('load', () => {
-    const canvas = document.getElementById('canvas');
+    const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext('2d')
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(10,20,150,50);
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let painting = false;
+
+    function startPosition(){
+        painting = true;
+    }
+
+    function finishedPosition(){
+        painting = false;
+    }
+
+    function draw(e){
+        if(!painting) return;
+        ctx.lineWidth=10;
+        ctx.lineCap=round;
+
+        ctx.lineTo(e.clientX, e.clientY)
+    }
 
 
+    canvas,addEventListener("mousedown", startPosition);
+    canvas,addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw)
 })
 
