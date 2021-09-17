f326b4019c07c9ac1ccba1b3dc8c8b4e9a9ce43b
Jbausc <jake.bausch2002@gmail.com>
2021-09-17 22:28:47 +1000

    maths

--- Radix.js
+++ Radix.js
@@ -1,4 +1,6 @@
-function myFunction() {
+function DecToBin() {
     var txt = document.getElementById("decimal").value;
     document.getElementById("DecToBin").innerHTML = txt;
   }
+
+document.getElementById("DecToBin").addEventListener("click", DecToBin);
