f79592a840165635c4969d3bdd2fc1effca1258f
Jbausc <jake.bausch2002@gmail.com>
2021-09-17 22:39:20 +1000

    testing

--- Radix.js
+++ Radix.js
@@ -2,5 +2,5 @@ function DecToBin() {
     var txt = document.getElementById("decimal").value;
     document.getElementById("DecToBin").innerHTML = txt;
   }
-
+  document.getElementById("DecToBin").innerHTML = "wow";
 document.getElementById("DecToBinButton").addEventListener("click", DecToBin);
