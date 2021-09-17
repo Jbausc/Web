a82b6e09661451f4425f082e190d5e34f759429e
Jbausc <jake.bausch2002@gmail.com>
2021-09-17 22:51:20 +1000

    te

--- Radix.js
+++
@@ -1,5 +0,0 @@
-function DecToBin() {
-    var txt = document.getElementById("decimal").value;
-    document.getElementById("DecToBin").innerHTML = txt;
-  }
-  document.getElementById("DecToBin").innerHTML = "wow";
