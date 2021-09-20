5d1beb47921af81ebb85d8bc64e8285bcb78cc60
Jbausc <jake.bausch2002@gmail.com>
2021-09-20 16:32:44 +1000

    Winput

--- GridStuff.js
+++ GridStuff.js
@@ -1,5 +1,9 @@
+document.getElementById("23").innerHTML="<img src=\"Car.png\">";
+var x = 23;
 document.addEventListener("keydown", function(e){
-    if (e.key=="W"){
-        document.getElementById("grid4").innerHTML="<img src=\"Untitled.png\">";
+    if (e.key=="w"){
+        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        x-=5;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
 })
