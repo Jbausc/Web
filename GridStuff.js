b5c48754ffb78fb73ceed25450b21db370125240
Jbausc <jake.bausch2002@gmail.com>
2021-09-20 19:41:01 +1000

    GrassAttempt1

--- GridStuff.js
+++ GridStuff.js
@@ -2,10 +2,12 @@ document.getElementById("1").innerHTML="<img src=\"Car.png\">";
 var x = 1;
 document.getElementById("50").innerHTML="<img src=\"Golf.png\">"
 document.addEventListener("keydown", Move);
-
+for(let i=2;i<50;i++){
+    document.getElementById(i.toString()).innerHTML="<img src=\"grass.png\">"; 
+}
 function Move(e){
     if ((e.key=="s") && ((x+10)<=50)){
-        document.getElementById(x.toString()).innerHTML=""; 
+        document.getElementById(x.toString()).innerHTML="<img src=\"grass.png\">"; 
         x+=10;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     } else if ((e.key=="w") && ((x-10)>0)){
