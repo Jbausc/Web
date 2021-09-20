c6b1e0eb35fef98f86db3d32e5c80d5a2273bde9
Jbausc <jake.bausch2002@gmail.com>
2021-09-20 16:59:15 +1000

    noNumbers

--- GridStuff.js
+++ GridStuff.js
@@ -2,28 +2,28 @@ document.getElementById("1").innerHTML="<img src=\"Car.png\">";
 var x = 1;
 document.addEventListener("keydown", function(e){
     if (e.key=="w"){
-        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        document.getElementById(x.toString()).innerHTML=""; 
         x-=10;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
 })
 document.addEventListener("keydown", function(e){
     if (e.key=="a"){
-        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        document.getElementById(x.toString()).innerHTML=""; 
         x-=1;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
 })
 document.addEventListener("keydown", function(e){
     if (e.key=="s"){
-        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        document.getElementById(x.toString()).innerHTML=""; 
         x+=10;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
 })
 document.addEventListener("keydown", function(e){
     if (e.key=="d"){
-        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        document.getElementById(x.toString()).innerHTML=""; 
         x+=1;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
