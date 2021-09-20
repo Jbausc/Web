e65bf08bd8623ff7824e647cc181e4ad0f0c9cdd
Jbausc <jake.bausch2002@gmail.com>
2021-09-20 19:19:17 +1000

    RewordFunctionTest

--- GridStuff.js
+++ GridStuff.js
@@ -1,44 +1,29 @@
 document.getElementById("1").innerHTML="<img src=\"Car.png\">";
 var x = 1;
 document.getElementById("50").innerHTML="<img src=\"Golf.png\">"
-document.addEventListener("keydown", function(e){
-    if ((e.key=="w") && ((x-10)>0)){
+document.addEventListener("keydown", Move);
 
-        document.getElementById(x.toString()).innerHTML=""; 
-        x-=10;
-        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
-        if(x==50){
-            document.getElementById("grid-container").innerHTML="Congrats";
-        }
-    }
-})
-document.addEventListener("keydown", function(e){
-    if ((e.key=="a") && (((x-1)%10)!=0) && ((x-1)!=0)){
-        document.getElementById(x.toString()).innerHTML=""; 
-        x-=1;
-        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
-        if(x==50){
-            document.getElementById("grid-container").innerHTML="Congrats";
-        }
-    }
-})
-document.addEventListener("keydown", function(e){
+function Move(event){
     if ((e.key=="s") && ((x+10)<=50)){
         document.getElementById(x.toString()).innerHTML=""; 
         x+=10;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
-        if(x==50){
-            document.getElementById("grid-container").innerHTML="Congrats";
-        }
-    }
-})
-document.addEventListener("keydown", function(e){
-    if ((e.key=="d") && (((x)%10)!=0)){
+    } else if ((e.key=="w") && ((x-10)>0)){
+
         document.getElementById(x.toString()).innerHTML=""; 
-        x+=1;
+        x-=10;
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
-        if(x==50){
-            document.getElementById("grid-container").innerHTML="Congrats";
-        }
+
+    } else if ((e.key=="d") && (((x)%10)!=0)){
+            document.getElementById(x.toString()).innerHTML=""; 
+            x+=1;
+            document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
+
+    } else if ((e.key=="a") && (((x-1)%10)!=0) && ((x-1)!=0)){
+        document.getElementById(x.toString()).innerHTML=""; 
+        x-=1;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
+        
     }
-})
+}
+    
