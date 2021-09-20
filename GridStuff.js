b905dff0702fa61b905b7d4f001a3b3087f51083
Jbausc <jake.bausch2002@gmail.com>
2021-09-20 16:35:18 +1000

    fourDirections

--- GridStuff.js
+++ GridStuff.js
@@ -7,3 +7,24 @@ document.addEventListener("keydown", function(e){
         document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
     }
 })
+document.addEventListener("keydown", function(e){
+    if (e.key=="a"){
+        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        x-=1;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
+    }
+})
+document.addEventListener("keydown", function(e){
+    if (e.key=="s"){
+        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        x+=5;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
+    }
+})
+document.addEventListener("keydown", function(e){
+    if (e.key=="d"){
+        document.getElementById(x.toString()).innerHTML=x.toString(); 
+        x+=1;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Car.png\">";
+    }
+})
