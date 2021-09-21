2831e874ab99e49b6bad25e06cf4ea0600525e7e
Jbausc <jake.bausch2002@gmail.com>
2021-09-21 12:08:25 +1000

    Test

--- ClickGame.js
+++ ClickGame.js
@@ -4,7 +4,7 @@ for(let i = 1; i<=10;i++){
 
 var blocks = [1,1,1,1,1,1,1,1,1,1]
 
-function breaking(e){
+function breaking(){
     blocks[parseInt(this.id)-1]+=1;
-    document.getElementById(this.id).innerHTML("<img src=\"Stage" + blocks[parseInt(this.id)-1] + ".png\">");
+    document.getElementById(this.id).innerHTML("<img src=\"Stage2.png\">");
 }
