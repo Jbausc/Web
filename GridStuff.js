be7b16a80fe8116fa8a8aa17c3c37f34925536f6
Jbausc <jake.bausch2002@gmail.com>
2021-09-21 17:46:01 +1000

    test

--- GridStuff.js
+++ GridStuff.js
@@ -1,11 +1,12 @@
 document.getElementById("1").innerHTML="<img src=\"Car.png\">";
 var x = 1;
-document.getElementById("10").innerHTML="<img src=\"Intro.png\">"
-document.getElementById("20").innerHTML="<img src=\"RadixCalc.png\">"
-document.getElementById("30").innerHTML="<img src=\"ClickGame.png\">"
+document.getElementById("10").innerHTML="<img src=\"Intro.png\">";
+document.getElementById("20").innerHTML="<img src=\"RadixCalc.png\">";
+document.getElementById("30").innerHTML="<img src=\"ClickGame.png\">" ;
 document.addEventListener("keydown", Move);
-for(let i=2;i<50;i++){
-    document.getElementById(i.toString()).innerHTML="<img src=\"grass.png\">"; 
+for(let i=2;i<=50;i++){
+    if(x!=10 && x!=20 && x!=30){document.getElementById(i.toString()).innerHTML="<img src=\"grass.png\">"; 
+    }
 }
 function Move(e){
     if ((e.key=="s") && ((x+10)<=50)){
