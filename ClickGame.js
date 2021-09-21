eb8c7c7fed1c95688a02e7fe4c8b960f155a2af8
Jbausc <jake.bausch2002@gmail.com>
2021-09-21 12:27:14 +1000

    fix

--- ClickGame.js
+++ ClickGame.js
@@ -2,10 +2,10 @@ for(let i = 1; i<=10;i++){
     document.getElementById("" + i).addEventListener("click", breaking);
 }
 
-var blocks = [1,1,1,1,1,1,1,1,1,1]
+var blocks = [1,1,1,1,1,1,1,1,1,1];
 
 function breaking(){
     var gridNum = parseInt(this.id);
     blocks[gridNum-1]+=1;
-    document.getElementById(this.id).innerHTML="<img src=\"Stage" + this.id +".png\">";
+    document.getElementById(this.id).innerHTML="<img src=\"Stage" + blocks[gridNum-1] +".png\">";
 }
