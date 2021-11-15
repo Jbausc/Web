825247cbfb4aac7f5c584322bb63e5270db14766
Jbausc <jake.bausch2002@gmail.com>
2021-11-15 22:43:55 +1000

    t

--- BigGame.js
+++ BigGame.js
@@ -1,5 +1,22 @@
 document.addEventListener("keydown", Move);
 var x = 220;
+
+for(let i = 120; i<=159;i++){
+    document.getElementById("" + i).addEventListener("click", breaking);
+}
+
+var blocks = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
+function breaking(){
+    var gridNum = parseInt(this.id);
+    if (blocks[gridNum-1]==4){
+    blocks[gridNum-1]=0;
+   }
+   blocks[gridNum-1]+=1;
+    document.getElementById(this.id).innerHTML="<img src=\"Stage" + blocks[gridNum-1] +".png\">";
+}
+
+
 function Move(e){
     if ((e.key=="a") && (document.getElementById((x+1).toString()).innerHTML=="<img src=\"Air.png\">")){
         document.getElementById(x.toString()).innerHTML="<img src=\"Air.png\">"; 
