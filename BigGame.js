db47eaeeb7ebb3b050fe59a83de4e706ea06fb29
Jbausc <jake.bausch2002@gmail.com>
2021-11-15 22:28:56 +1000

    t

---
+++ BigGame.js
@@ -0,0 +1,15 @@
+document.addEventListener("keydown", Move);
+var x = 221;
+function Move(e){
+    if ((e.key=="d") && (document.getElementById((x+1).toString()).innerHTML=="<img src=\"Air.png\">")){
+        document.getElementById(x.toString()).innerHTML="<img src=\"Air.png\">"; 
+        x+=1;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Guy.png\">";
+
+    } else if ((e.key=="a") && (document.getElementById((x-1).toString()).innerHTML=="<img src=\"Air.png\">")){
+        document.getElementById(x.toString()).innerHTML="<img src=\"Air.png\">"; 
+        x-=1;
+        document.getElementById(x.toString()).innerHTML="<img src=\"Guy.png\">";
+        
+    }
+}
